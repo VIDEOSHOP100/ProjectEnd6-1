e051e379daa7bda61354e633aab8d2fdcf4e32c6
EEIT100Team5 <38684094+EEIT100Team5@users.noreply.github.com>
2018-05-13 17:50:35 +0800

    編輯結帳 頁面

--- EEIT/src/main/java/com/iii/_16_/OrderSystem/Order/controller/OrderController.java
+++ EEIT/src/main/java/com/iii/_16_/OrderSystem/Order/controller/OrderController.java
@@ -1,5 +1,15 @@
 package com.iii._16_.OrderSystem.Order.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class OrderController {
-	
+	@RequestMapping(value = "/order.do", method = RequestMethod.GET)
+	public String checkOut() {
+		System.out.println("checkout");
+		
+		return "OrderSystem/checkoutpage";
+	}
 }
