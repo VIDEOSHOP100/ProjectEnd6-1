68c24ab1ce337f4a09b1169d2692692c7d7eac67
AXBXSD <axbxsd10@gmail.com>
2018-05-17 16:47:44 +0800

    意見後台50%

--- EEIT/src/main/webapp/WEB-INF/views/backstage/js/backstageAdvice.js
+++ EEIT/src/main/webapp/WEB-INF/views/backstage/js/backstageAdvice.js
@@ -13,30 +13,31 @@ $(document).ready(function () {
 
         $('#content').prepend($('<h2></h2>').text(title));
         $('#content> p').remove();
-        if (title == '會員客訴') {
+        
+
+        if(title == '回覆留言') {
             $.ajax({
                 type: "GET",
-                url: "/EEIT/getAllMemberReport",
+                url: "/EEIT/getAllAdviceReport",
                 dataType: "json",
                 timeout: 600000,
   
                 success: function (data) {
-                    var tab = $('<table></table>').addClass('table table-striped').html('<thead><tr><th class="text-center">投訴編號</th><th>投訴人</th><th>被投訴人</th><th>標題</th><th>投訴時間</th><th>投訴狀態</th></tr></thead><tbody></tbody>');
+                    var tab = $('<table></table>').addClass('table table-striped').html('<thead><tr><th class="text-center">意見編號</th><th>投書人</th><th>標題</th><th>投書時間</th><th>投書狀態</th></tr></thead><tbody></tbody>');
                     $('#content>h2').after(tab);
                         var docFrag = $(document.createDocumentFragment());
                         console.log(data);
-                        $.each(data, function (idx,MemberReport) {
+                        $.each(data, function (idx,AdviceReport) {
 
-                            var cell1 = $('<td></td>').text(MemberReport.memberReportSeqNo).addClass('pointer text-primary text-center memberReportSeqNo');
-                            var cell2 = $('<td></td>').text(MemberReport.accountFrom);
-                            var cell3 = $('<td></td>').text(MemberReport.accountTo);
-                            var cell4 = $('<td></td>').text(MemberReport.reportTitle);
-                            var ret = new Date(MemberReport.reportTime);
+                            var cell1 = $('<td></td>').text(AdviceReport.memberReportSeqNo).addClass('pointer text-primary text-center AdviceReportSeqNo');
+                            var cell2 = $('<td></td>').text(AdviceReport.account);
+                            var cell3 = $('<td></td>').text(AdviceReport.adviceTitle);
+                            var ret = new Date(AdviceReport.adviceTime);
                             var formated = ret.getFullYear() + '/' +(ret.getMonth()+1) +'/' +ret.getDate() + ' ' +ret.getHours() + ':' +ret.getMinutes()
-                            var cell5 = $('<td></td>').text(formated);
-                            var cell6 = $('<td></td>').text(MemberReport.reportStatus);
+                            var cell4 = $('<td></td>').text(formated);
+                            var cell5 = $('<td></td>').text(AdviceReport.adviceStatus);
 
-                            var row = $('<tr></tr>').append([cell1, cell2, cell3, cell4, cell5, cell6]);
+                            var row = $('<tr></tr>').append([cell1, cell2, cell3, cell4, cell5]);
 
                             docFrag.append(row);
 
@@ -50,6 +51,7 @@ $(document).ready(function () {
             })
 
         }
+        
         
         
     }
@@ -108,6 +110,60 @@ $(document).ready(function () {
     	})
   	
     })
+    
+    var arSN;
+    
+    $(document).on("click",'.AdviceReportSeqNo',function(){
+    	mrSN = $(this).text();
+    	    	
+    	$('#content').empty();
+    	    	
+    	$.ajax({
+    		
+    		type:"GET",
+    		url:"/EEIT/adviceReport/" + arSN,
+    		
+    		success:function(data){
+    			var ret = new Date(data.reportTime);
+    			var formated = ret.getFullYear() + '/' +(ret.getMonth()+1) +'/' +ret.getDate() + ' ' +ret.getHours() + ':' +ret.getMinutes()
+    			var format = $('<container></container>').html(
+    					'<row>' +
+    						'<div class="col-lg-8">' +
+    							'<h2 class="mt-4">' + data.reportTitle + '</h2>' +
+    							'<p class="lead"> 檢舉人 : ' +  
+    								'<a  target="_blank" href="/EEIT/profile/' +data.accountFrom + '">' + data.accountFrom + '</a>' + 
+    								'  被檢舉人 : ' + 
+    								'<a  target="_blank" href="/EEIT/profile/' +data.accountTo + '">' + data.accountTo + '</a>' +
+    							'</p>' +
+    							'<p>Posted on' + formated + '</p><hr>' +
+    							'<p>' + data.reportContent +'</p><hr>' +
+    							'<div class="card my-4"><h5 class="card-header">回覆檢舉</h5><div class="card-body">' +
+    							'<form:form method="POST" action="/replyMemberReport" modelAttribute="MemberReportBean" class = "form-horizontal">' + 
+    							'<textarea id="replyMRContent" path="replyContent" class="form-control" rows="3"></textarea>' + 
+    							'<button class = "btn btn-primary" id = "replyMemberReportButton">回覆檢舉</button>' +
+    							'</form>' +
+    							'</div>'+
+    							'</div>'+
+    							'</div>'+
+    					'</row>'
+    			
+    				) 
+    					
+    				$('#content').append(format);
+      				
+    		},
+    		error: function (xhr, ajaxOptions, thrownError) {
+        
+                alert(thrownError);
+            
+        },
+   
+    		
+    	})
+  	
+    })
+    
+    
     
     
     
