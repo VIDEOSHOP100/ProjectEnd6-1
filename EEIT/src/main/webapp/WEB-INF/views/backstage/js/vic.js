54711d78a4adceaecafc8d4d15271971f878ada9
vic1529 <vic2012coke@gmail.com>
2018-05-24 16:50:45 +0800

    常見問題集SQL DLC

--- EEIT/src/main/webapp/WEB-INF/views/backstage/js/vic.js
+++ EEIT/src/main/webapp/WEB-INF/views/backstage/js/vic.js
@@ -1,91 +1,139 @@
-
-
 $(document).ready(function () {
 
-	loadProduct(1);
-	
-//	<table id="QuestionList" class="table table-bordered">
-//	<thead>
-//		<tr>
-//			<th>產品編號</th>
-//			<th>產品名稱</th>
-//			<th>產品價格</th>
-//			<th>庫存量</th>
-//			<th>管理</th>
-//		</tr>
-//	</thead>
-//	<tbody>
+    $('.list-group-item').click(selectCategory);
+    		
+    		
+    	function selectCategory() {
 
-	   //新增產品
-	    $('#buttonAdd').click(function(){
-	    	var datas = $('form[name="myForm"]').serialize();
-	    	$.post('ProductsInsert',datas,function(data){
-	    		alert(data);	 		    		
-	    		loadProduct(1);	 		    		
-	    		$('#questionListSeqNo').val('');
-				$('#questionListTitle').val('');
-				$('#questionListArticle').val('');
-				$('#questionListStatus').val('');
-				$('#questionListUploadDate').val('');
-				$('#managerId').val('');
-				$('#questionListType').val('');
-	    	});
-	    })
-	   
-	    //讀取產品
-	   function loadProduct(id){
+        $('.list-group-item').removeClass('bg-primary').removeClass('text-white').addClass('text-primary');
+        $(this).removeClass('text-primary').addClass('bg-primary').addClass('text-white');
+        $('#content').empty();
 
-	    	 $.getJSON('Products', { 'categoryID': id }, function (datas) {			          
-		           var docFrag = $(document.createDocumentFragment());
-		           var tb = $('#productTable>tbody');
-		           tb.empty();
-		           $.each(datas, function (idx,product) {
-		               var cell1 = $('<td></td>').text(product.ProductID);
-		               var cell2 = $('<td></td>').text(product.ProductName);
-		               var cell3 = $('<td></td>').text(product.UnitPrice);
-		               var cell4 = $('<td></td>').text(product.UnitsInStock);
-		               var cell5 = $('<td></td>').html('<button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button> <button class="btn btn-info"><i class="fas fa-edit"></i></button>');
+        var title = $(this).text();
 
-		               var row = $('<tr></tr>').append([cell1, cell2, cell3, cell4,cell5]);
-		               docFrag.append(row);
-		           })
-		           tb.append(docFrag);
-		       })	   
-	   }
+        $('#content').prepend($('<h2></h2>').text(title));
+        $('#content> p').remove();
+        if (title == '問答集管理') {
+            $.ajax({
+                type: "GET",
+                url: "/EEIT/QusetList",
+                dataType: "json",
+                timeout: 600000,
+  
+                success: function (data) {
+                    var tab = $('<table></table>').addClass('table table-striped').html('<thead><tr><th class="text-center">投訴編號</th><th>投訴人</th><th>被投訴人</th><th>標題</th><th>投訴時間</th><th>投訴狀態</th></tr></thead><tbody></tbody>');
+                    $('#content>h2').after(tab);
+                        var docFrag = $(document.createDocumentFragment());
+                        console.log(data);
+                        $.each(data, function (idx,MemberReport) {
 
-	   $('#productTable>tbody').on('click','tr button:nth-child(1)',function(){
-		   var id = $(this).parents('tr').find('td:nth-child(1)').text();
-		   $.get('ProductsDelete',{ProductID:id},function(data){
-			   alert(data);
-			   loadProduct(1);
-		   })
-	  })
-	    
-	   $('#productTable>tbody').on('click','tr button:nth-child(2)',function(){
-		   var ProductID = $(this).parents('tr').find('td:nth-child(1)').text();
-		   var ProductName = $(this).parents('tr').find('td:nth-child(2)').text();
-		   var UnitPrice = $(this).parents('tr').find('td:nth-child(3)').text();
-		   var UnitsInStock = $(this).parents('tr').find('td:nth-child(4)').text();
-		  
-		   $('#ProductID').val(ProductID).next('span').text(ProductID);
-		   $('#ProductName').val(ProductName);
-		   $('#UnitPrice').val(UnitPrice);
-		   $('#UnitsInStock').val(UnitsInStock);
-	   })
-	   
-	   
-	   //更新產品
-	   $('#buttonUpdate').click(function(){
+                            var cell1 = $('<td></td>').text(MemberReport.memberReportSeqNo).addClass('pointer text-primary text-center memberReportSeqNo');
+                            var cell2 = $('<td></td>').text(MemberReport.accountFrom);
+                            var cell3 = $('<td></td>').text(MemberReport.accountTo);
+                            var cell4 = $('<td></td>').text(MemberReport.reportTitle);
+                            var ret = new Date(MemberReport.reportTime);
+                            var formated = ret.getFullYear() + '/' +(ret.getMonth()+1) +'/' +ret.getDate() + ' ' +ret.getHours() + ':' +ret.getMinutes()
+                            var cell5 = $('<td></td>').text(formated);
+                            var cell6 = $('<td></td>').text(MemberReport.reportStatus);
 
-		   var datas = $('form[name="myForm"]').serialize();
-	    	$.post('ProductsUpdate',datas,function(data){
-	    		alert(data);
-	    		loadProduct(1);
-	    		 $('#ProductID').val('').next('span').text('');
-				   $('#ProductName').val('');
-				   $('#UnitPrice').val('');
-				   $('#UnitsInStock').val('');
-	    	});
-	   });
+                            var row = $('<tr></tr>').append([cell1, cell2, cell3, cell4, cell5, cell6]);
 
+                            docFrag.append(row);
+
+                        })
+                         $('#content tbody').html(docFrag);
+                },
+
+                error: function (xhr, ajaxOptions, thrownError) {
+                        alert(thrownError);
+                },
+            })
+
+        }
+        
+        
+    }
+    
+//   ---------------------------------以上功能列選擇------------------------------------- 
+
+    
+    	var mrSN;
+    
+    $(document).on("click",'.memberReportSeqNo',function(){
+    	mrSN = $(this).text();
+    	    	
+    	$('#content').empty();
+    	    	
+    	$.ajax({
+    		
+    		type:"GET",
+    		url:"/EEIT/memberReport/" + mrSN,
+    		
+    		success:function(data){
+    			var ret = new Date(data.reportTime);
+    			var formated = ret.getFullYear() + '/' +(ret.getMonth()+1) +'/' +ret.getDate() + ' ' +ret.getHours() + ':' +ret.getMinutes()
+    			var format = $('<container></container>').html(
+    					'<row>' +
+    						'<div class="col-lg-8">' +
+    							'<h2 class="mt-4">' + data.reportTitle + '</h2>' +
+    							'<p class="lead"> 檢舉人 : ' +  
+    								'<a  target="_blank" href="/EEIT/profile/' +data.accountFrom + '">' + data.accountFrom + '</a>' + 
+    								'  被檢舉人 : ' + 
+    								'<a  target="_blank" href="/EEIT/profile/' +data.accountTo + '">' + data.accountTo + '</a>' +
+    							'</p>' +
+    							'<p>Posted on' + formated + '</p><hr>' +
+    							'<p>' + data.reportContent +'</p><hr>' +
+    							'<div class="card my-4"><h5 class="card-header">回覆檢舉</h5><div class="card-body">' +
+    							'<form:form method="POST" action="/replyMemberReport" modelAttribute="MemberReportBean" class = "form-horizontal">' + 
+    							'<textarea id="replyMRContent" path="replyContent" class="form-control" rows="3"></textarea>' + 
+    							'<button class = "btn btn-primary" id = "replyMemberReportButton">回覆檢舉</button>' +
+    							'</form>' +
+    							'</div>'+
+    							'</div>'+
+    							'</div>'+
+    					'</row>'
+    			
+    				) 
+    					
+    				$('#content').append(format);
+      				
+    		},
+    		error: function (xhr, ajaxOptions, thrownError) {
+        
+                alert(thrownError);
+            
+        },
+   
+    		
+    	})
+  	
+    })
+    
+    
+    $(document).on("click",'#replyMemberReportButton',function(){
+    	
+    	
+    	var reply = $('#replyMRContent').val();
+    	
+    	$.ajax({
+    		
+    		type:"POST",
+    		url:"/EEIT/memberReport",
+    		data:{ _method : "PUT" ,memberReportSeqNo:mrSN ,replyContent:reply},
+    		
+    		success:function(){
+    		
+    			alert('回覆投訴完成!');
+    			$('.bg-primary').trigger("click");
+    		
+    		},
+    		
+    		error:function(e){
+    			alert(e);
+    		}
+    		
+    	})
+    	
+    })
+    
 })
