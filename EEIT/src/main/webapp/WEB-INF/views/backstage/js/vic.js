e9991ab2046cb3bd20b9f7f5b356657fcd04f3e7
vic1529 <vic2012coke@gmail.com>
2018-05-25 09:46:03 +0800

    0515 commit

--- EEIT/src/main/webapp/WEB-INF/views/backstage/js/vic.js
+++ EEIT/src/main/webapp/WEB-INF/views/backstage/js/vic.js
@@ -16,42 +16,63 @@ $(document).ready(function () {
         if (title == '問答集管理') {
             $.ajax({
                 type: "GET",
-                url: "/EEIT/QusetList",
+                url: "/EEIT/QuestionList/getAllQuestionList",
                 dataType: "json",
                 timeout: 600000,
   
                 success: function (data) {
-                    var tab = $('<table></table>').addClass('table table-striped').html('<thead><tr><th class="text-center">投訴編號</th><th>投訴人</th><th>被投訴人</th><th>標題</th><th>投訴時間</th><th>投訴狀態</th></tr></thead><tbody></tbody>');
+                    var tab = $('<table></table>').addClass('table table-striped').html('<thead><tr><th class="text-center">問題編號</th><th>編輯人員</th><th>問題類型</th><th>標題</th><th>上傳時間</th><th>是否顯示</th></tr></thead><tbody></tbody>');
                     $('#content>h2').after(tab);
                         var docFrag = $(document.createDocumentFragment());
                         console.log(data);
-                        $.each(data, function (idx,MemberReport) {
+                        $.each(data, function (idx,QuestList) {
 
-                            var cell1 = $('<td></td>').text(MemberReport.memberReportSeqNo).addClass('pointer text-primary text-center memberReportSeqNo');
-                            var cell2 = $('<td></td>').text(MemberReport.accountFrom);
-                            var cell3 = $('<td></td>').text(MemberReport.accountTo);
-                            var cell4 = $('<td></td>').text(MemberReport.reportTitle);
-                            var ret = new Date(MemberReport.reportTime);
+                            var cell1 = $('<td></td>').text(QuestList.questionListSeqNo).addClass('pointer text-primary text-center questionListSeqNo');
+                            var cell2 = $('<td></td>').text(QuestList.managerId);
+                            
+                            if(QuestList.questionListType == 1){
+                            	var cell3 = $('<td></td>').text("帳號問題");
+                            }else if(QuestList.questionListType == 2){
+                            	var cell3 = $('<td></td>').text("密碼與登入");
+                            }else if(QuestList.questionListType == 3){
+                            	var cell3 = $('<td></td>').text("建立帳號與刪除帳號");
+                            }else if(QuestList.questionListType == 4){
+                            	var cell3 = $('<td></td>').text("商品交易");
+                        	}else if(QuestList.questionListType == 5){
+                        		var cell3 = $('<td></td>').text("直播拍賣");
+                        	}else if(QuestList.questionListType == 6){
+                        		var cell3 = $('<td></td>').text("影片上傳");
+                        	}else if(QuestList.questionListType == 7){
+                        		var cell3 = $('<td></td>').text("聯絡我們");
+                        	}
+                            
+                            var cell4 = $('<td></td>').text(QuestList.questionListTitle);
+                            
+                            var ret = new Date(QuestList.questionListUploadDate);
                             var formated = ret.getFullYear() + '/' +(ret.getMonth()+1) +'/' +ret.getDate() + ' ' +ret.getHours() + ':' +ret.getMinutes()
                             var cell5 = $('<td></td>').text(formated);
-                            var cell6 = $('<td></td>').text(MemberReport.reportStatus);
+                            
+                            if(QuestList.questionListStatus == 1){
+                            	var cell6 = $('<td></td>').text("顯示");
+                            }else{
+                            	var cell6 = $('<td></td>').text("不顯示");
+                            }
 
                             var row = $('<tr></tr>').append([cell1, cell2, cell3, cell4, cell5, cell6]);
 
                             docFrag.append(row);
-
-                        })
+                            
+                           })
                          $('#content tbody').html(docFrag);
-                },
+                    },
 
-                error: function (xhr, ajaxOptions, thrownError) {
+                        error: function (xhr, ajaxOptions, thrownError) {
                         alert(thrownError);
-                },
-            })
-
-        }
-        
-        
+                    },
+               })
+  
+          }
+              
     }
     
 //   ---------------------------------以上功能列選擇------------------------------------- 
@@ -59,7 +80,7 @@ $(document).ready(function () {
     
     	var mrSN;
     
-    $(document).on("click",'.memberReportSeqNo',function(){
+    $(document).on("click",'.questionListSeqNo',function(){
     	mrSN = $(this).text();
     	    	
     	$('#content').empty();
@@ -67,7 +88,7 @@ $(document).ready(function () {
     	$.ajax({
     		
     		type:"GET",
-    		url:"/EEIT/memberReport/" + mrSN,
+    		url:"/EEIT/QuestionList/" + mrSN,
     		
     		success:function(data){
     			var ret = new Date(data.reportTime);
