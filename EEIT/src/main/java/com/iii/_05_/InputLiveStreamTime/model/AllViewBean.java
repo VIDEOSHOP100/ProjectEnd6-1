7b1cc7fffd7ca93d7f8f09216755af6f481bf5e1
張紘笙 <35601322+strike5931@users.noreply.github.com>
2018-05-25 20:37:17 +0800

    PPPUSH

---
+++ EEIT/src/main/java/com/iii/_05_/InputLiveStreamTime/model/AllViewBean.java
@@ -0,0 +1,18 @@
+//package com.iii._05_.InputLiveStreamTime.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "LiveStream")
+//public class AllViewBean {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Integer liveStreamSeqNo;
+//	
+//	
+//	
+//}
