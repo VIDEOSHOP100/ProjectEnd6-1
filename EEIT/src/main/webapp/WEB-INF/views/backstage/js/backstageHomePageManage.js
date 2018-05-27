ca3f6b9f286983fac40bff17e521f270850c41d3
AXBXSD <axbxsd10@gmail.com>
2018-05-27 16:20:14 +0800

    首頁後台介面修改

--- EEIT/src/main/webapp/WEB-INF/views/backstage/js/backstageHomePageManage.js
+++ EEIT/src/main/webapp/WEB-INF/views/backstage/js/backstageHomePageManage.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
   
                 success: function (data) {
                 	var list = data.VideoBeanList;
-                	var row = $('<row></row>');
+                	var row = $('<div></div>').css({'height':'620px','overflow-x':'scroll'});
                 	var docFrag = $(document.createDocumentFragment());
                 	$.each(list,function(idx,videoBean){
                 		var cell = $('<img class="col-md-3 mt-2 choose">').attr('src','/EEIT/getImage/video/'+videoBean.videoSeqNo).attr('title',videoBean.videoTitle).attr('VideoSeqNo',videoBean.videoSeqNo);
